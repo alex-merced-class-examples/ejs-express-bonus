761cc120e60815a9e0de2fc974b31c83a31b39df
Alex Merced <alex@alexmerced.dev>
2024-01-09 20:42:19 -0500

    update

--- public/app.js
+++ public/app.js
@@ -1,30 +1,41 @@
-const $pokemonList = $("pokemon-listing")
-
-$pokemonList.on("click", (event) => {
-    const $item = $(event.currentTarget)
-    console.log($item)
-    const id = $item.attr("data-index")
-
-    fetch(`/pokemon/${id}`)
-    .then(response => response.json())
-    .then(data => {
+////////////////////////////////////////////
+// code for pokemon/index
+/////////////////////////////////////////
 
 
-        const $rightWin = $("right-screen") 
-        // $rightWin.empty()
+const pokeIndex = document.querySelector("#pokemonindex")
 
-        $rightWin.html(`<div>
-            <h3>${data.name}</h3>
-            <img src="${data.img}">
-            <p>Type: ${data.type}</p>
-            <a href="/pokemon/${id}/edit"><button>Edit</button></a>
-            <form action="/pokemon/${id}?_method=DELETE" method="post">
-            <button>Delete</button>
-            </form>
-        
-        </div>`)
+if(pokeIndex){
+    const $pokemonList = $("pokemon-listing")
 
-
-
+    $pokemonList.on("click", (event) => {
+        const $item = $(event.currentTarget)
+        console.log($item)
+        const id = $item.attr("data-index")
+    
+        fetch(`/pokemon/${id}`)
+        .then(response => response.json())
+        .then(data => {
+    
+    
+            const $rightWin = $("right-screen") 
+            // $rightWin.empty()
+    
+            $rightWin.html(`<div>
+                <h3>${data.name}</h3>
+                <img src="${data.img}">
+                <p>Type: ${data.type}</p>
+                <a href="/pokemon/${id}/edit"><button>Edit</button></a>
+                <form action="/pokemon/${id}?_method=DELETE" method="post">
+                <button>Delete</button>
+                </form>
+            
+            </div>`)
+    
+    
+    
+        })
     })
-})
+}
+
+
